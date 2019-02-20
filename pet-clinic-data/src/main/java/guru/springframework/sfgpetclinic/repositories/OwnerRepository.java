f582f0db1e5d733cc149b4e427f2554116a21c23
lbm-a <lynnarts11@gmail.com>
2019-02-20 14:41:27 +0100

    #54 impl find owner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,12 @@ package guru.springframework.sfgpetclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import guru.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
