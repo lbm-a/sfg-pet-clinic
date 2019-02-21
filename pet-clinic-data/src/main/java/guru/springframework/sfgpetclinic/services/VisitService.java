aa0f41e637b0d2d1b7938fa0da9b90d74349ab44
lbm-a <lynnarts11@gmail.com>
2019-02-21 11:27:09 +0100

    added visit controller and mapping sg#57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Visit;
-import org.springframework.data.repository.CrudRepository;
 
 public interface VisitService extends CrudService<Visit, Long> {
 }
